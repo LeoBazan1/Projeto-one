79b10a252d717e2e8b725c8964b07c3ee9c450a7
LeoBazan1 <leonardobazanrocha2001@gmail.com>
2025-02-08 14:16:26 -0300

    Projeto - Amigos v.1.1.0

--- app.js
+++ app.js
@@ -36,3 +36,37 @@ function atualizarListaAmigos() {
         listaAmigos.appendChild(li);
     });
 }
+
+function exibirAmigos() {
+    
+    const lista = document.getElementById('listaAmigos');
+
+    
+    lista.innerHTML = '';
+
+   
+    for (let i = 0; i < amigos.length; i++) {
+    
+        const li = document.createElement('li');
+        
+    
+        li.textContent = amigos[i];
+
+    
+        lista.appendChild(li);
+    }
+}
+
+
+function adicionarAmigo() {
+    const input = document.getElementById('amigo');
+    const nomeAmigo = input.value.trim();
+
+    if (nomeAmigo !== '') {
+        amigos.push(nomeAmigo);
+        input.value = ''; 
+        exibirAmigos(); 
+    } else {
+        alert('Por favor, insira um nome.');
+    }
+}
